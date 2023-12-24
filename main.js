adfba860f57ede2e55026d94ea581b12fc997186
buggydhunter <myprivate827@gmail.com>
2023-12-24 13:05:12 +0100

    crawl single page with error handling

---
+++ main.js
@@ -0,0 +1,18 @@
+const {crawlPage} = require('./crawl.js')
+
+function main(){
+    if (process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    } 
+    if (process.argv.length > 3) {
+        console.log("too many cli args")
+        process.exit(1)
+    } 
+
+    const baseUrl = process.argv[2]
+    console.log(`starting crawl ${baseUrl}`)
+    crawlPage(baseUrl)
+}
+
+main()
